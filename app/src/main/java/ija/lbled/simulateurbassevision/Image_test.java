41c619f19534e6177fae2f603dcb86e9083c5c6b
Lucas BLED <lucas.bled12@gmail.com>
2015-05-22 15:02:56 +0200

    Modification interface + ajout sélection photos

--- app/src/main/java/ija/lbled/simulateurbassevision/Image_test.java
+++ app/src/main/java/ija/lbled/simulateurbassevision/Image_test.java
@@ -10,8 +10,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Created by l.bled on 19/05/2015.
@@ -34,22 +44,27 @@ public class Image_test extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.application_activity);
 
-        ((Button) findViewById(R.id.Button01))
-                .setOnClickListener(new OnClickListener() {
+        //Valeurs Acuite : 0.1, 0.2, 0.3, 0.4, 0.5, 0.63, 0.8, 1/10, 1.3/10, 1.6/10, 2/10, 2.5/10, 3.2/10, 4/10
+        //Valeurs Distance : 4m, 2.5m, 40cm, 10cm
 
-                    public void onClick(View arg0) {
+        //fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-                        // in onCreate or any event where your want the user to
-                        // select a file
-                        Intent intent = new Intent();
-                        intent.setType("image/*");
-                        intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(Intent.createChooser(intent,
-                                "Select Picture"), SELECT_PICTURE);
-                    }
-                });
+        findViewById(R.id.button_importer).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // in onCreate or any event where your want the user to
+                // select a file
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -57,6 +72,25 @@ public class Image_test extends Activity {
             if (requestCode == SELECT_PICTURE) {
                 Uri selectedImageUri = data.getData();
                 selectedImagePath = getPath(selectedImageUri);
+                ImageView image = (ImageView) findViewById(R.id.image);
+                Bitmap bMap = BitmapFactory.decodeFile(selectedImagePath);
+                Log.i("taille", "hauteur"+bMap.getHeight());
+                Log.i("taille", "largeur" + bMap.getWidth());
+                Log.i("taille", "taille " + 1920 * (bMap.getHeight() / bMap.getWidth()));
+                if (bMap.getHeight() < bMap.getWidth()) {
+                    image.setImageBitmap(Bitmap.createScaledBitmap(bMap, 1920, 1080, false));
+                } else {
+                    image.setImageBitmap(Bitmap.createScaledBitmap(bMap, 1080, 1920, false));
+                }
+               /* Mat imageMat = new Mat ( bMap.getHeight(), bMap.getWidth(), CvType.CV_8U, new Scalar(4));
+                Bitmap myBitmap32 = bMap.copy(Bitmap.Config.ARGB_8888, true);
+                Utils.bitmapToMat(myBitmap32, imageMat);
+                Utils.bitmapToMat(bMap, imageMat);
+                Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_RGB2GRAY, 4);
+                Log.i("Path", selectedImagePath);
+                Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(), imageMat.rows(),Bitmap.Config.ARGB_8888);;
+                Utils.matToBitmap(imageMat, resultBitmap);*/
+               // image.setImageBitmap(bMap);
             }
         }
     }
