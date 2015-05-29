c87795f586c6462db2941f365cc1f95123075089
Lucas BLED <lucas.bled12@gmail.com>
2015-05-29 16:13:31 +0200

    Contraste ok + ajout cadre Champ Visuel

--- app/src/main/java/ija/lbled/simulateurbassevision/Controleur.java
+++ app/src/main/java/ija/lbled/simulateurbassevision/Controleur.java
@@ -152,19 +152,7 @@ public class Controleur extends Activity {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 if (imageChargee) {
                     bMap = original.copy(Bitmap.Config.ARGB_8888, true);
-                    Mat imageMat = new Mat(bMap.getHeight(), bMap.getWidth(), CvType.CV_8UC1);
-                    Utils.bitmapToMat(bMap, imageMat);
-                    /*int valeur = 0;
-                    if (seekBar.getProgress() > 50) {
-                        valeur = (seekBar.getProgress() - 50) * 2;
-                    } else if (seekBar.getProgress() < 50) {
-                        valeur = 0 - ((50 - seekBar.getProgress())*2);
-                    } else {
-                        valeur = 0;
-                    }*/
-                    //valeurContraste = 1 - seekBar.getProgress()/100;
-                    imageMat.convertTo(imageMat, -1, 0.5, valeurLumi);
-                    Utils.matToBitmap(imageMat, bMap);
+                    bMap = createContrast(bMap, 0 - ((100 - (seekBar.getProgress())) * 0.8));
                     image.setImageBitmap(bMap);
                 }
             }
@@ -227,6 +215,48 @@ public class Controleur extends Activity {
                 imageChargee = true;
             }
         }
+    }
+
+    public static Bitmap createContrast(Bitmap src, double value) {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // color information
+        int A, R, G, B;
+        int pixel;
+        // get contrast value
+        double contrast = Math.pow((100 + value) / 100, 2);
+
+        // scan through all pixels
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                // apply filter contrast for every channel R, G, B
+                R = Color.red(pixel);
+                R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(R < 0) { R = 0; }
+                else if(R > 255) { R = 255; }
+
+                G = Color.green(pixel);
+                G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(G < 0) { G = 0; }
+                else if(G > 255) { G = 255; }
+
+                B = Color.blue(pixel);
+                B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(B < 0) { B = 0; }
+                else if(B > 255) { B = 255; }
+
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+        // return final image
+        return bmOut;
     }
 
     /**
---
+++ app/src/main/java/ija/lbled/simulateurbassevision/Vue.java
@@ -0,0 +1,7 @@
+package ija.lbled.simulateurbassevision;
+
+/**
+ * Created by l.bled on 28/05/2015.
+ */
+public class Vue {
+}
